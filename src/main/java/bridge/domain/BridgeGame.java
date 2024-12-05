7517236646b7ae522d36535d2aa51dcb14d55c71
Sumi Chung <drsoombre@naver.com>
2024-12-05 14:20:57 +0900

    fix: BridgeMaker 변경사항 삭제

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -49,34 +49,4 @@ public class BridgeGame {
     public void retry() {
     }
 
-    /**
-     * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
-     */
-    public static class BridgeMaker {
-
-        private final BridgeRandomNumberGenerator bridgeNumberGenerator;
-
-        public BridgeMaker(BridgeRandomNumberGenerator bridgeNumberGenerator) {
-            this.bridgeNumberGenerator = bridgeNumberGenerator;
-        }
-
-        /**
-         * @param size 다리의 길이
-         * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-         */
-        public List<String> makeBridge(int size) {
-            List<String> bridge = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                bridge.add(returnBridgeParse());
-            }
-            return bridge;
-        }
-
-        private String returnBridgeParse() {
-            if (bridgeNumberGenerator.generate() == 0) {
-                return "D";
-            }
-            return "U";
-        }
-    }
 }
